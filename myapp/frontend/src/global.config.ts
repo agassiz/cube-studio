0645f0900b787fc48a79daeecb67dccf560577da
andyhome <andyhome163@163.com>
2024-03-17 16:06:05 +0800

    删除logo

--- myapp/frontend/src/global.config.ts
+++ myapp/frontend/src/global.config.ts
@@ -1,7 +1,7 @@
 import { TThemeType } from "./theme"
 
-const appLogo = require('./images/logoCB.svg')
-const loadingLogo = require('./images/logoCB.svg')
+const appLogo = "";//require('./images/logoCB.svg')
+const loadingLogo ="" // require('./images/logoCB.svg')
 
 interface IGlobalConfig {
     appLogo: any,
